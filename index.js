1fdb6d28824ed606b72df9323c55506a523ca81b
mahfuj10 <mahfujglobal@gmail.com>
2021-12-02 12:27:10 +0600

    add new method

--- index.js
+++ index.js
@@ -84,10 +84,10 @@ async function run() {
       res.send(result)
     })
     // delete event
-    app.delete('/event/:id', async (req, res) => {
+    app.delete('/admins/:id', async (req, res) => {
       const id = req.params.id;
       const query = { _id: ObjectId(id) };
-      const result = await eventCollection.deleteOne(query);
+      const result = await adminCollection.deleteOne(query);
       res.send(result)
     })
 
